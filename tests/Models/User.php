fa2888ea5c04e56637b0784aa4fcb0985446a618
rennokki <rennokki@gmail.com>
2021-07-16 21:35:35 +0300

    Update User.php

--- tests/Models/User.php
+++ tests/Models/User.php
@@ -2,13 +2,10 @@
 
 namespace RenokiGames\:package_namespace\Test\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasFactory;
-
     protected $fillable = [
         'name', 'email', 'password',
     ];
